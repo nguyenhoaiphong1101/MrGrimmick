64a2e987aee390c40c84f4241140869408a35e47
= <=>
2021-07-27 07:11:54 +0700

    Last commit 2

--- GameMrGrimmick/05-ScenceManager/Fish.cpp
+++ GameMrGrimmick/05-ScenceManager/Fish.cpp
@@ -41,52 +41,6 @@ void Fish::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
     }
     x += dx;
 
-    //for (UINT i = 0; i < coObjects->size(); i++)
-   //{
-   // LPGAMEOBJECT obj = coObjects->at(i);
-
-    //}
-   //CalcPotentialCollisions(coObjects, coEvents);
-   ////// No collision occured, proceed normally
-   //if (coEvents.size() == 0)
-   //{
-
-    // x += dx;
-   // y += dy;
-
-    //}
-   //else
-   //{
-
-    //// // land ...fly
-   // float min_tx, min_ty, nx = 0, ny;
-   // float rdx = 0;
-   // float rdy = 0;
-   // FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
-
-    // x += min_tx * dx + nx * 0.4f;
-   // y += min_ty * dy + ny * 0.4f;
-
-    //// /*if (nx!=0) vx = 0;*/
-   // if (ny != 0) vy = 0;
-
-    //// // Collision logic with other objects
-   //// //
-   // for (UINT i = 0; i < coEventsResult.size(); i++)
-   // {
-   // LPCOLLISIONEVENT e = coEventsResult[i];
-
-    // if (e->nx != 0 && ny == 0)
-   // {
-   // this->vx = -this->vx;
-   // this->nx = -this->nx;
-
-    // }
-   // }
-   //}
-
-    ////// clean up collision events
-   //for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
 }
 
 void Fish::Render()
